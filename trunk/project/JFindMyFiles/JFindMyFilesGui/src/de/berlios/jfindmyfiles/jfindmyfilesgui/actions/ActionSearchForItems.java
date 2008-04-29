63facd2d48fa5930f124bc730daac14d87daa940
tica <tica@4bb6def2-df44-0410-9887-c888bde5ae40>
2008-04-29 14:52:48 +0000

    Elaborados menus

---
+++ trunk/project/JFindMyFiles/JFindMyFilesGui/src/de/berlios/jfindmyfiles/jfindmyfilesgui/actions/ActionSearchForItems.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.berlios.jfindmyfiles.jfindmyfilesgui.actions;
+
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.CallableSystemAction;
+
+public final class ActionSearchForItems extends CallableSystemAction {
+
+    public void performAction() {
+    // TODO implement action body
+    }
+
+    public String getName() {
+        return NbBundle.getMessage(ActionSearchForItems.class, "CTL_ActionSearchForItems");
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        // see org.openide.util.actions.SystemAction.iconResource() Javadoc for more details
+        putValue("noIconInMenu", Boolean.TRUE);
+    }
+
+    public HelpCtx getHelpCtx() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+
+    @Override
+    protected boolean asynchronous() {
+        return false;
+    }
+}
